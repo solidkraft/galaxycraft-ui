ec2b9f5fbc3cc35ac94db4583cff41ecca53c737
Dan <dansification@gmail.com>
2023-09-04 09:47:10 -0400

    List Quests on a different way

--- src/pages/Quests.js
+++ src/pages/Quests.js
@@ -1,26 +1,25 @@
 import { useMemo } from "react";
-import { Project, Words, Link } from "arwes";
+import { useParams } from "react-router-dom"
+import { Project, Content, Link } from "arwes";
 
 const Quests = props => {
+  const journeyId = useParams().journey_id;
   const truncate = (input) =>
     input?.length > 300 ? `${input.substring(0, 254)}...` : input;
   const questsList = useMemo(() => {
     return props.quests?.map(quest =>
       <>
-      <Link href={`/quests/${quest.id}/story`}  key={quest.id} >
-        <Project
-          animate
-          header={quest.name}
-        >
-          {anim => (
-              <p><Words animate show={anim.entered}>
-                  {truncate(quest.task.body)}
-              </Words>
-              </p>
-          )}
-        </Project>
-      </Link>
-      <br />
+        <Link href={`/quests/${quest.id}/story`}  key={quest.id} >
+          <Project
+            animate
+            header={quest.name}
+          >
+            {anim => (
+              <Content animate show={anim.entered} dangerouslySetInnerHTML={{ __html: truncate(quest.story?.body) }} />
+            )}
+          </Project>
+        </Link>
+        <br />
       </>
     );
   }, [props.quests]);
