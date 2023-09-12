e2e82ea14d8c3266939b040c662ec6c3cce2baa6
Dan <dansification@gmail.com>
2023-09-12 11:42:56 -0400

    Next Lesson

--- src/pages/QuestDetails.js
+++ src/pages/QuestDetails.js
@@ -10,7 +10,7 @@ const QuestDetails = (props) => {
   const quest = useQuestDetails(questId);
   const questDetails = useMemo(() => quest, [quest]);
 
-  const currentLesson = 5;
+  const currentLesson = 6;
   const shouldNotRenderContent = questId > currentLesson;
 
   if (shouldNotRenderContent) {
